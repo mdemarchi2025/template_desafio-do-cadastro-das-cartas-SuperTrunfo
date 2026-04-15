d69e27903ab62279d57c6da5c11f95ba9b89dbd1
MARCOS TADEU DEMARCHI <marcosdemarchi2012@gmail.com>
2026-04-15 22:22:00 +0000

    Apresentando o Desafio Super Trunfo Novato

--- SUPER_TRUNFO/CartasSuperTrunfo.c
+++ SUPER_TRUNFO/CartasSuperTrunfo.c
@@ -4,7 +4,7 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 // Este programa é um exemplo simples de um jogo de Super Trunfo em C.
-
+// Enviado pela terceira vez
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
     
